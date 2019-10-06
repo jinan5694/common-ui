44f187033da2203f5c982de7cbcde9f443b3bf99
jinan <jinan5694@users.noreply.github.com>
2019-10-06 11:09:42 +0800

    demo

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Play from './views/Play.vue'
+import Demo from './views/Demo.vue'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
   routes: [
-    { path: '/play', component: Play }
+    { path: '/play', component: Play },
+    { path: '/demo', component: Demo }
   ]
 })
---
+++ src/views/Demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="demo">
+    <div class="list">
+      <div class="item" v-for="item in 100" :key="item">{{item}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+.list {
+  height: 300px;
+  width: 200px;
+  overflow-y: auto;
+}
+.item {
+  height: 40px;
+}
+</style>
